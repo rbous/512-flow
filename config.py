e06400e66008501791ba27b6425d9a08c7f07087
rbous <rboussanni@gmail.com>
2024-09-28 13:32:05 -0400

    restructuring of project

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
-# Known nodes in the network
-KNOWN_NODES = [
-    ('127.0.0.1', 5001),
-    ('127.0.0.1', 5002),
-    ('127.0.0.1', 5003),
-    ('127.0.0.1', 5004)
-]
-
 # Maximum chunk size for file sharing
 CHUNK_SIZE = 512
